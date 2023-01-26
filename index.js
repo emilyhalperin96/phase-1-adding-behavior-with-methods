a41a23fc75d3f8a6ca0994c2ba3c53572a6a08a1
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-01-26 12:45:02 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+//each class accepts the parameters name and sex
+//stores those values as properties
+
+//create the method speak for each class 
+// cat, speak returns "name says meow"
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog {
+
+ constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+      speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+  constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        if (this.sex == 'male') {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+    }
